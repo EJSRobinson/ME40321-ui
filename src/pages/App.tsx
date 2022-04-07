2e44577d5bee19d3201f76ce838b4630ef15e17a
EJSRobinson <elliotjs.robinson@gmail.com>
2022-04-07 10:06:10 +0100

    add single input column comp

--- src/pages/App.tsx
+++ src/pages/App.tsx
@@ -3,22 +3,9 @@ import InputCard from '../components/input-card';
 import OutputCard from '../components/output-card';
 import AddConstraintDialog from '../components/add-constraint-dialog';
 import Button from '@mui/material/Button';
-
-const constraints: Array<string> = [];
-
-const addConstraint = (propName: string) => {
-  constraints.push(propName);
-  console.log(constraints);
-};
-
-const removeConstraint = (propName: string) => {
-  constraints.splice(constraints.indexOf(propName), 1);
-  console.log(constraints);
-};
+import InputColumn from '../components/input-column';
 
 function App() {
-  const [addConstOpenFlag, setAddConstOpenFlag] = useState(false);
-
   const testValue = {
     typeName: 'quant',
     max: 12345,
@@ -32,37 +19,9 @@ function App() {
 
   return (
     <>
-      <InputCard
-        propName={'Static Stability Margin'}
-        propType={'quant'}
-        units={'kg'}
-        remover={removeConstraint}
-      />
       <OutputCard propName={'Lift Force'} propType={'quant'} units={'N'} quantValue={testValue} />
       <OutputCard propName={'Material'} propType={'quant'} units={'None'} qualValue={testValue2} />
-      {constraints?.map((constraint) => {
-        return (
-          <InputCard
-            propName={constraint}
-            propType={'quant'}
-            units={'kg'}
-            remover={removeConstraint}
-          />
-        );
-      })}
-      <Button
-        variant='outlined'
-        onClick={() => {
-          setAddConstOpenFlag(true);
-        }}
-      >
-        Add new constraint
-      </Button>
-      <AddConstraintDialog
-        open={addConstOpenFlag}
-        onClose={addConstraint}
-        openFlagSetter={setAddConstOpenFlag}
-      />
+      <InputColumn />
     </>
   );
 }
